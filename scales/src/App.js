0cdb43c1f7533e5bd64d41e0a9ab639eec17f08e
Katie Walters <katie_walters@live.co.uk>
2022-09-23 18:51:25 +0100

    changed favicon

--- scales/src/App.js
+++ scales/src/App.js
@@ -3,23 +3,12 @@ import './App.css';
 
 function App() {
 
-  // useEffect(() => {
-
     async function fetchScales() {
       const response = await fetch('http://localhost:3000/api/getAll');
       const scales = await response.json();
       console.log(scales)
-      // return movies;
     }
     fetchScales()
-    // fetchMoviesJSON().then(movies => {
-    //   movies; // fetched movies
-    // });
-
- 
-// });
-   
-
 
 return (
   <div className="App">
