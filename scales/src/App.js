e1a4b5869611ecc0af2e799b46f2c8555ea51dfc
Katie Walters <katie_walters@live.co.uk>
2022-09-24 18:27:19 +0100

    updated logic

--- scales/src/App.js
+++ scales/src/App.js
@@ -1,6 +1,23 @@
 import * as React from "react";
 import "./App.css";
 import Form from "./Components/selectInstrument";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f50057',
+    },
+    accent: {
+      main: '#ffff00',
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#11cb5f',
+    },
+  },
+});
+
 
 function App() {
 
@@ -8,9 +25,14 @@ function App() {
 
     return (
         <>
+        <ThemeProvider theme={theme}>
+
             <div className="App">
+           
                 <Form />
+           
             </div>
+        </ThemeProvider>
         </>
     );
 }
--- scales/src/Components/showScale.js
+++ scales/src/Components/showScale.js
@@ -1,4 +1,39 @@
 import React, { useEffect } from "react";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
+const card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        Word of the Day
+      </Typography>
+      <Typography variant="h5" component="div">
+        Hello
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        adjective
+      </Typography>
+      <Typography variant="body2">
+        well meaning and kindly.
+        <br />
+        {'"a benevolent smile"'}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+  </React.Fragment>
+);
+
+
+
+
 
 const ShowScale = ({ instrument, grade }) => {
     const [scales, setScales] = React.useState("");
@@ -7,35 +42,38 @@ const ShowScale = ({ instrument, grade }) => {
         const fetchScales = async () => {
             const response = await fetch("http://localhost:3000/api/getAll");
             const data = await response.json();
-            console.log("data", data);
+            // console.log("data", data);
             setScales(data);
         };
         fetchScales();
     }, []);
-
+    // console.log("state scales", scales);
     const showScales = () => {
         console.log("instrument", instrument);
         console.log("grade", grade);
         //check that state has been saved
         if (instrument !== "" && grade !== "") {
-            console.log("instrument and grade have been selected");
+            // console.log("instrument and grade have been selected");
             
             return (
                 <div>
                     {scales.map((scale) => {
                         if (scale.Grade === grade ) {
                         console.log("scale.grade:", scale.Grade, "grade:", grade)
-                        console.log(scale.Majors.length)
+                        
                         //length of majors array
-                        let x = Math.floor(Math.random() * scale.Majors.length);
+                        let x = Math.floor(Math.random() * scale.Scales.length);
                         console.log(x)
                         return (
-                            <div key={scale.id}>
-                                <h2>{scale.Majors[x].Key}</h2>
-                                <h2>{scale.Majors[x].Range}</h2>
+                            <><Box sx={{ minWidth: 275 }}>
+                                <Card variant="outlined">{card}</Card>
+                            </Box><div key={scale.id}>
+                                    <h2>{scale.Scales[x].Key}</h2>
+                                    <h2>Range: {scale.Scales[x].Range}</h2>
 
-                                <hr />
-                            </div>
+                                    <hr />
+                                </div></>
+               
                         );
                         }
                     })}
@@ -43,10 +81,15 @@ const ShowScale = ({ instrument, grade }) => {
             );
         }
     };
-    showScales();
+    // showScales();
 
-    console.log("state scales", scales);
-    return <div>{showScales()}</div>;
+
+    return <div>
+    
+
+    {showScales()}
+     
+    <button onClick={showScales}>Pick a Scale</button></div>;
 };
 
 export default ShowScale;
